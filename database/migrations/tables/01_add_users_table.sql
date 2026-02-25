a45ffdfb62e127073cda70bf86b2181db327c938
jullian richard-devin <205008919+JullianRD@users.noreply.github.com>
2026-02-25 15:57:13 +0100

    add user_table

---
+++ database/migrations/tables/01_add_users_table.sql
@@ -0,0 +1,43 @@
+-- Relation Merise : Entité indépendante
+-- ============================================================================
+
+-- Forcer l'encodage client
+SET CLIENT_ENCODING TO 'UTF8';
+
+CREATE TABLE IF NOT EXISTS users (
+    id_user UUID DEFAULT uuidv7 () PRIMARY KEY,
+    email CITEXT UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    pseudo CITEXT VARCHAR(50) UNIQUE NOT NULL,
+    biographie CITEXT,
+    role_name role_enum NOT NULL DEFAULT 'visiteur',
+    auth_provider auth_provider_enum NOT NULL DEFAULT 'local',
+    settings_user JSONB NOT NULL DEFAULT '{}',
+    gdpr_consent BOOLEAN NOT NULL DEFAULT FALSE,
+    gdpr_consent_date TIMESTAMPTZ,
+    created_at TIMESTAMPTZ NOT NUll DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMPTZ,
+    CONSTRAINT chk_email_is_valid CHECK ( -- Contraintes métier
+        email ~ '^[^@]+@[^@.]+\.[^@]+$'
+    )
+);
+
+-- Indexation JSONB (Recherche rapide dans les réglages utilisateur)
+CREATE INDEX idx_users_settings ON users USING gin (settings_user);
+-- "Je crée un index spécial optimisé pour rechercher rapidement dans ma colonne JSONB settings_user."
+
+--Doccumentation
+
+COMMENT ON TABLE users IS 'Stocke les informations d identification et les préférences des utilisateurs (RGPD compliant)';
+
+COMMENT ON COLUMN users.settings_user IS 'Préférences utilisateur en JSONB : {"theme": "dark", "language": "fr", "notifications": true}';
+
+COMMENT ON COLUMN users.gdpr_consent IS 'Consentement RGPD obligatoire pour créer un compte';
+
+COMMENT ON COLUMN users.gdpr_consent_date IS 'Date et heure précises du consentement RGPD';
+
+-- Application du trigger (écouteur d'événement) “Quand il se passe X dans la table, je lance automatiquement une fonction.”
+CREATE TRIGGER set_timestamp_users BEFORE UPDATE ON users FOR EACH ROW EXECUTE PROCEDURE trigger_set_timestamp();
+-- “À chaque fois qu’un utilisateur est modifié, je mets automatiquement à jour son timestamp.”
+
+
